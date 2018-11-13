c087bad31a383da36a3bb968415874788f6bb8ab
Nam <nam2611@gmail.com>
2018-11-13 20:21:38 +0700

    Update

---
+++ Assets/Script/PopulateGrid.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class PopulateGrid : MonoBehaviour
+{
+    public GameObject prefab; // This is our prefab object that will be exposed in the inspector
+   
+    public int numberToCreate; // number of objects to create. Exposed in inspector
+
+    public int poolSize;
+    private GameObject[] UIviewImage ;
+    public int i = 0, j = 11;
+    public RectTransform rectTransform;
+    public float currentViewPosition = 0f, nextViewPosition = 0f, previousViewPosition = 0f;
+    public float temp;
+    void Start()
+    {
+        UIviewImage = new GameObject[poolSize];
+        Populate();
+        rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(400, numberToCreate * (100 + 5));
+        nextViewPosition = 105;
+    }
+
+    void Update()
+    {
+
+        if (i >= poolSize)
+        {
+            i = 0;
+        } else if (i < 0)
+        {
+            i = poolSize - 1;
+        }
+        if (rectTransform.localPosition.y - currentViewPosition > 310f && j < numberToCreate) 
+        {
+            temp = - nextViewPosition - 105 *10;
+            UIviewImage[i].GetComponent<RectTransform>().localPosition = new Vector3(55, temp, 0);
+            previousViewPosition = currentViewPosition;
+            currentViewPosition = nextViewPosition;
+            nextViewPosition += 105f; 
+            j++;
+            i++;
+        }
+        // need to fix this 
+        else if ((rectTransform.localPosition.y - previousViewPosition < -155f) && j >11) //right here
+        {
+
+            temp = -currentViewPosition;
+            UIviewImage[i].GetComponent<RectTransform>().localPosition = new Vector3(55, temp, 0);
+            previousViewPosition = currentViewPosition;
+            currentViewPosition -=105f;
+            i--;
+            j--;
+        }
+
+    }
+
+    void Populate()
+    {
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            // Create new instances of our prefab until we've created as many as we specified
+            UIviewImage[i] = (GameObject)Instantiate(prefab, transform);
+            //UIviewImage[i].GetComponent<Image>().color = Random.ColorHSV();
+        }
+        
+    }
+}
---
+++ Assets/Script/test.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(UnityEngine.UI.Image))]
+
+// only work at the cost of more processing (improve UX)
+public class test : MonoBehaviour 
+{
+    UnityEngine.UI.Image _img;
+    //http://www.pooyak.com/p/progjpeg/jpegload.cgi?o=1
+    //http://bbshowcase.org/progressive/?r=4&progressive=330438&rate=0
+    private string url = "http://bbshowcase.org/progressive/?r=4&progressive=330438&rate=0";
+    private UnityWebRequest wr;
+    private Texture2D texture;
+    private Sprite sprite;
+    private DownloadHandlerTexture texDl;
+    private bool isPause = false;
+    private void Start()
+    {
+        _img = GetComponent<UnityEngine.UI.Image>();
+
+    }
+
+    private void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Download(url);
+            StartCoroutine(Downloadupdate());
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            isPause = true;
+            StartCoroutine(outViewUpdate());
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isPause = false;
+        }
+
+
+
+
+    }
+
+
+    public void Download (string _url)
+    {
+
+        texture = new Texture2D(2, 2);
+        wr = new UnityWebRequest(_url);
+        texDl = new DownloadHandlerTexture(true);
+        wr.downloadHandler = texDl;
+        wr.chunkedTransfer = true;
+        wr.SendWebRequest();
+        sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 1f);
+        _img.sprite = sprite;
+        //del_action += Downloadupdate;
+    }
+
+    IEnumerator Downloadupdate()
+    {
+        while (!wr.isDone)
+        {
+            while (isPause && !wr.isDone)
+            {
+                yield return null;
+            }
+            if (wr.downloadProgress > 0.1f)
+            {
+                texture.LoadImage(texDl.data); //need something else SLOW really SLOW
+            }
+            yield return null;
+        }
+        texture.LoadImage(texDl.data);
+        texture = texDl.texture;
+    }
+    IEnumerator outViewUpdate()
+    {
+        while (!wr.isDone)
+        {
+            yield return null;
+        }
+        texture.LoadImage(texDl.data);
+
+    }
+
+}

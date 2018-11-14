72171daafad6524d4bad63b140be62f617276848
Nam <nam2611@gmail.com>
2018-11-14 21:35:18 +0700

    update

---
+++ Assets/Script/MainChar.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainChar : MonoBehaviour
+{
+    public class Character : MainCharecterObject
+    {
+        public override int SetHealth
+        {
+            get
+            {
+                Debug.Log("GET; Abstract, must override in inheritated class");
+                return Health;
+            }
+            set
+            {
+                Debug.Log("SET; Abstract, must override in inheritated class");
+                Health = value;
+            }
+        }
+
+        public void getBom ()
+        {
+            Debug.Log(BomCount);
+            Debug.Log("extra from inheritated class");
+           
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Character main = new Character();
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            main.SetHealth = 100;
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            int temp = main.SetHealth;
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            main.getBom();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            main.getBom();
+        }
+
+    }
+}
---
+++ Assets/Script/MainCharecterObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MainCharecterObject
+{
+    protected int Health =0;
+    protected int BomCount =0;
+
+    virtual public void AddBom()
+    {
+        Debug.Log("Invoke from abstract");
+        BomCount++;
+    }
+
+    virtual public void UseBom()
+    {
+        Debug.Log("Invoke from abstract");
+        BomCount--;
+    }
+    public abstract int SetHealth
+    {
+        get;
+        set;
+    }
+
+}
+public class testing
+{
+
+}
